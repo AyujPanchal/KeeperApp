c05754d26e761b378f368bccf05e63db7bcaf69f
AyujPanchal <b19099@students.iitmandi.ac.in>
2021-03-21 20:50:02 +0530

    Add date.js and modify app.js

--- app.js
+++ app.js
@@ -2,30 +2,24 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname+'/date.js');
 
 const app = express();
 
-let items = ["Buy Food","Cook Food","Eat Food"];
-let workItems = [];
+const items = ["Buy Food","Cook Food","Eat Food"];
+const workItems = [];
 
 app.set("view engine","ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.get('/',function(req, res){
-    var today = new Date();
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    var day = today.toLocaleDateString("en-US", options);
-
+const day = date.getDate();
     res.render("list",{listTitle: day, newListItems: items});
 });
 
 app.post('/',function(req, res){
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     if(req.body.list === "Work"){
         workItems.push(item);
         res.redirect("/work"); 
