c7e808b5f0535e1fa43da8981e74fa5b2e91c752
AyujPanchal <b19099@students.iitmandi.ac.in>
2021-03-24 19:44:11 +0530

    Add feature to delete custom lists

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 
@@ -52,7 +53,7 @@ app.get('/',function(req, res){
 });
 
 app.get("/:customListName", function(req, res){
-    const customListName=req.params.customListName;
+    const customListName=_.capitalize(req.params.customListName);
     List.findOne({name: customListName}, function(err, foundList){
         if(!err){
             if(!foundList){
@@ -72,21 +73,40 @@ app.get("/:customListName", function(req, res){
 
 app.post('/',function(req, res){
     const itemName = req.body.newItem;
+    const listName = req.body.list;
     const item = new Item({
         name: itemName
     })
-    item.save();
-    res.redirect("/");
+    if(listName === "Today"){
+        item.save();
+        res.redirect("/");    
+    } else{
+        List.findOne({name: listName}, function(err, foundList){
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect("/" + listName);
+        })
+    }
 });
 
 app.post("/delete", function(req, res){
     const checkedItemId=req.body.checkbox;
-    Item.findByIdAndRemove(checkedItemId, function(err){
-        if(!err){
-            console.log("Successfully deleted checked item.");
-            res.redirect("/");
-        }
-    });
+    const listName = req.body.listName;
+    if(listName==="Today"){
+        Item.findByIdAndRemove(checkedItemId, function(err){
+            if(!err){
+                console.log("Successfully deleted checked item.");
+                res.redirect("/");
+            }
+        });
+    } else{
+        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+            if(!err){
+                res.redirect("/" + listName);
+            }
+        });
+    }
+
 });
 
 // app.post("/work", function(req, res){
